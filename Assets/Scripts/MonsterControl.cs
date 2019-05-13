907828fcdeeab26005052ac2fc16c6ab36530e10
cgrets <camilla.gretsch@bluewin.ch>
2019-05-13 17:31:14 +1000

    Monster following player

---
+++ Assets/Scripts/MonsterControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterControl : MonoBehaviour
+{
+
+    private GameObject player;
+    private Rigidbody2D rigidbody;
+    private Vector2 targetPosition;
+    private float distance = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetPosition = new Vector2(player.transform.position.x - distance, player.transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, 5.0f * Time.deltaTime);
+    }
+}
