16f4a52210453605afb67b28bc3156e23734e4ad
Zuguang Chen <airborn@qq.com>
2019-05-15 21:40:15 +1000

    Add Enemy 2 types to gameplay
    1. Type 1 going forward and no turning around
    
    2. Type 2 going back and forth.

---
+++ Assets/Scripts/StunEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StunEnemy : MonoBehaviour
+{
+    [SerializeField] GameObject deathVFX;
+    [SerializeField] float bounceForce;
+
+    private Rigidbody2D playerRigidbody2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody2D = transform.parent.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+
+            Instantiate(deathVFX, other.transform.position, other.transform.rotation);
+
+            playerRigidbody2D.velocity = new Vector3(playerRigidbody2D.velocity.x, bounceForce, 0f);
+        }
+    }
+}
