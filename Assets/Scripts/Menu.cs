4074692a14d64308a25c39b287146bfb05bcb4d2
Zuguang Chen <airborn@qq.com>
2019-05-18 22:56:42 +1000

    Add Menu Screen

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    [SerializeField] string tutorialLevel;
+    [SerializeField] string continueLevel;
+
+    [SerializeField] int startingLifeNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(tutorialLevel);
+
+        PlayerPrefs.SetInt("ScoreCount", 0);
+        PlayerPrefs.SetInt("LifeCount", startingLifeNumber);
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene(continueLevel);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
