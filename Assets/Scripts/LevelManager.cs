28b4d1a49d37cc178f900778e2d7ca18a846f733
Zuguang Chen <airborn@qq.com>
2019-06-01 20:12:45 +1000

    Add damage and jump audio to Tutorial, 1, 2, 3 level

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -277,6 +277,7 @@ public class LevelManager : MonoBehaviour
             if (healthCount > 0)
             {
                 thePlayer.PushedBack();
+                thePlayer.damageAudio.Play();
             }
         }
     }
