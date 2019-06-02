14678e7112427341c209d22dd3c2b97d4da87522
Zuguang Chen <airborn@qq.com>
2019-06-02 23:16:23 +1000

    Level 1 modification & tut level design hint in description
    also I delete tut level enemies and obstacles, because this will introduce in level1, tut level just focus on movement.

---
+++ Assets/Scripts/MoveUpDown.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpDown : MonoBehaviour
+{
+    [SerializeField] Transform upPoint;
+    [SerializeField] Transform downtPoint;
+    [SerializeField] float moveSpeed;
+    [SerializeField] bool movingDown = false;
+
+    private Rigidbody2D myRigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovingBoundary();
+
+        MovingSpeed();
+    }
+
+    private void MovingSpeed()
+    {
+        if (movingDown)
+        {
+            myRigidbody2D.velocity = new Vector3(myRigidbody2D.velocity.x, moveSpeed, 0f);
+        }
+        else
+        {
+            myRigidbody2D.velocity = new Vector3(-myRigidbody2D.velocity.x, -moveSpeed, 0f);
+        }
+    }
+
+    private void MovingBoundary()
+    {
+        if (movingDown && transform.position.y > downtPoint.position.x)
+        {
+            movingDown = true;
+        }
+
+        if (!movingDown && transform.position.y < upPoint.position.x)
+        {
+            movingDown = false;
+        }
+    }
+}
